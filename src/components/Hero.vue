27bf4b130d6f2bd691003b5fe755db11b410fc8b
Linuxpert <g.manfre10@gmail.com>
2021-11-23 15:33:30 +0100

    aggiunta bonus

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hello">
+  <div>
     <img src="https://i0.wp.com/www.thebigblogtheory.it/wp-content/uploads/2016/09/marvel_vs_dc_poster_by_deffectx-d95a5a8.jpg?fit=810%2C523&ssl=1" alt="">
   </div>
 </template>
